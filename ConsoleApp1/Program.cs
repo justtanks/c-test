b66638b39af6dab8c373736cf55005ddd58805e5
邵伟 <just_tanks@163.com>
2018-09-27 14:07:10 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 100; i++) {
+                Console.WriteLine(i + "hello world");
+            }
+             
+        }
+    }
+
+}
